adbab05f8f92651a0c8c970e860dfc40a93b2837
codelando <codelando@gmail.com>
2023-08-29 22:37:56 -0300

    Added simple validator class

---
+++ Validator.php
@@ -0,0 +1,15 @@
+<?php
+
+class Validator 
+{
+    // As a pure function string() can be made static with no risks
+    public static function string($value, $min = 1, $max = INF) { 
+        $value = trim($value);
+
+        return strlen($value) >= $min && strlen($value) <= $max;
+    }
+
+    public static function email($value) {
+        return filter_var($value, FILTER_VALIDATE_EMAIL);
+    }
+}
