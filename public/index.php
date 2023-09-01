53cb03bcf85558f734230c107e4a2fdf9e42f6b6
codelando <codelando@gmail.com>
2023-08-31 22:16:25 -0300

    Added service container for the database and bootstrap file for the project.

--- public/index.php
+++ public/index.php
@@ -7,15 +7,17 @@ require BASE_PATH . "Core/functions.php";
 spl_autoload_register(function ($class) {
     
     $class = str_replace('\\', '/', $class);
-
+    
     require base_path("{$class}.php");
 });
 
 $router = new \Core\Router();
 
 require base_path('routes.php');
+require base_path('bootstrap.php');
 
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 $method = $_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
+
 
 $router->route($uri, $method);
