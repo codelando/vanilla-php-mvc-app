5ff8407ccb28ef5fca044c1801825e773785bd94
codelando <codelando@gmail.com>
2023-09-02 00:50:41 -0300

    Added middlewares support (auth / guest) for routes and password hashing.

--- routes.php
+++ routes.php
@@ -4,16 +4,16 @@ $router->get('/', 'controllers/index.php');
 $router->get('/about', 'controllers/about.php');
 $router->get('/contact', 'controllers/contact.php');
 
-$router->get('/register', 'controllers/register/create.php');
+$router->get('/register', 'controllers/register/create.php')->only('guest');
 $router->post('/register', 'controllers/register/store.php');
 
-$router->get('/notes', 'controllers/notes/index.php');
-$router->get('/note', 'controllers/notes/show.php');
-$router->delete('/notes', 'controllers/notes/destroy.php');
+$router->get('/notes', 'controllers/notes/index.php')->only('auth');
+$router->get('/note', 'controllers/notes/show.php')->only('auth');
+$router->delete('/notes', 'controllers/notes/destroy.php')->only('auth');
 
-$router->get('/notes/create', 'controllers/notes/create.php');
-$router->get('/notes/edit', 'controllers/notes/edit.php');
-$router->patch('/note', 'controllers/notes/update.php');
+$router->get('/notes/create', 'controllers/notes/create.php')->only('auth');
+$router->get('/notes/edit', 'controllers/notes/edit.php')->only('auth');
+$router->patch('/note', 'controllers/notes/update.php')->only('auth');
 
 
 $router->post('/notes', 'controllers/notes/store.php');
