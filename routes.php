f515b01345319aa2ad2fcfc0364bb811f3f4a28a
codelando <codelando@gmail.com>
2023-09-03 18:37:46 -0300

    Created Http directory for controllers and form validation. Added form validation class.

--- routes.php
+++ routes.php
@@ -1,23 +1,23 @@
 <?php
 
-$router->get('/', 'controllers/index.php');
-$router->get('/about', 'controllers/about.php');
-$router->get('/contact', 'controllers/contact.php');
+$router->get('/', 'index.php');
+$router->get('/about', 'about.php');
+$router->get('/contact', 'contact.php');
 
-$router->get('/login', 'controllers/sessions/create.php')->only('guest');
-$router->post('/sessions', 'controllers/sessions/store.php')->only('guest');
-$router->delete('/sessions', 'controllers/sessions/destroy.php')->only('auth');
+$router->get('/login', 'sessions/create.php')->only('guest');
+$router->post('/sessions', 'sessions/store.php')->only('guest');
+$router->delete('/sessions', 'sessions/destroy.php')->only('auth');
 
-$router->get('/register', 'controllers/register/create.php')->only('guest');
-$router->post('/register', 'controllers/register/store.php')->only('guest');
+$router->get('/register', 'register/create.php')->only('guest');
+$router->post('/register', 'register/store.php')->only('guest');
 
-$router->get('/notes', 'controllers/notes/index.php')->only('auth');
-$router->get('/note', 'controllers/notes/show.php')->only('auth');
+$router->get('/notes', 'notes/index.php')->only('auth');
+$router->get('/note', 'notes/show.php')->only('auth');
 
-$router->get('/notes/create', 'controllers/notes/create.php')->only('auth');
-$router->patch('/note', 'controllers/notes/update.php')->only('auth');
+$router->get('/notes/create', 'notes/create.php')->only('auth');
+$router->patch('/note', 'notes/update.php')->only('auth');
 
-$router->get('/notes/edit', 'controllers/notes/edit.php')->only('auth');
-$router->post('/notes', 'controllers/notes/store.php')->only('auth');
+$router->get('/notes/edit', 'notes/edit.php')->only('auth');
+$router->post('/notes', 'notes/store.php')->only('auth');
 
-$router->delete('/notes', 'controllers/notes/destroy.php')->only('auth');
+$router->delete('/notes', 'notes/destroy.php')->only('auth');
