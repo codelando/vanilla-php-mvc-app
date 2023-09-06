718fe04bcecfd0436ff28fd696dab4f9c86798ef
codelando <codelando@gmail.com>
2023-09-05 21:46:26 -0300

    Adding Composer and it's autoloader;

--- public/index.php
+++ public/index.php
@@ -1,18 +1,13 @@
 <?php
 
+const BASE_PATH = __DIR__ . '/../';
+
+require BASE_PATH . "vendor/autoload.php";
+
 use Core\Session;
 use Core\ValidationException;
 
-const BASE_PATH = __DIR__ . '/../';
-
 require BASE_PATH . "Core/functions.php";
-
-spl_autoload_register(function ($class) {
-    
-    $class = str_replace('\\', '/', $class);
-    
-    require base_path("{$class}.php");
-});
 
 $router = new \Core\Router();
 
